9ead851ed6518559ed23c1eea56410566e418000
leonardoozelin <leonardoozelin@gmail.com>
2018-04-26 17:26:21 -0300

    Update 26-04

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MatFormFieldModule, MatFormFieldControl} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+    options: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      'fixed': false,
+      'top': 0,
+      'bottom': 0,
+    });
+  }
+
+  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
 }
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 
+// ANGULAR MATERIAL IMPORTS
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule, MatFormFieldControl} from '@angular/material/form-field';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {MatInputModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FormBuilder],
+  bootstrap: [AppComponent],
 })
+
 export class AppModule { }
